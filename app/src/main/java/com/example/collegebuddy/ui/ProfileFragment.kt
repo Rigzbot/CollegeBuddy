0d21fc8831d1cee87b75c9095afa905cd34930a5
Rishik Gupta <rishikgupta8@gmail.com>
2022-01-04 16:33:42 +0530

    added logout button in profile fragment

--- app/src/main/java/com/example/collegebuddy/ui/ProfileFragment.kt
+++ app/src/main/java/com/example/collegebuddy/ui/ProfileFragment.kt
@@ -1,13 +1,17 @@
 package com.example.collegebuddy.ui
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.viewModels
 import com.example.collegebuddy.databinding.FragmentProfileBinding
+import com.example.collegebuddy.ui.login.LoginActivity
 import com.example.collegebuddy.viewModels.ProfileViewModel
+import com.google.firebase.auth.FirebaseAuth
 import dagger.hilt.android.AndroidEntryPoint
 
 class ProfileFragment : Fragment() {
@@ -17,6 +21,8 @@ class ProfileFragment : Fragment() {
 
     private val viewModel: ProfileViewModel by viewModels()
 
+    private lateinit var mAuth: FirebaseAuth
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -24,7 +30,23 @@ class ProfileFragment : Fragment() {
         _binding = FragmentProfileBinding.inflate(inflater)
         binding.lifecycleOwner = viewLifecycleOwner
 
+        mAuth = FirebaseAuth.getInstance()
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        setupViews()
+    }
+
+    private fun setupViews() {
+        binding.logoutButton.setOnClickListener {
+            mAuth.signOut()
+
+            Toast.makeText(context, "Logged out Successfully!", Toast.LENGTH_SHORT).show()
+            startActivity(Intent(requireContext(), LoginActivity::class.java))
+        }
     }
 
     override fun onDestroy() {
